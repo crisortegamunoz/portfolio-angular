b3a057734fe8b846db231bcd61d5e9208aed166a
Cristian Ortega <crisortegamunoz@gmail.com>
2023-06-07 18:16:51 -0400

    Configuracion para deployar el proyecto en firebase hosting, se añaden dos nuevas librerias una para realizar la paginación y otra para tener un spinner para cargar la data

--- src/app/shared/components/footer/footer.component.ts
+++ src/app/shared/components/footer/footer.component.ts
@@ -14,7 +14,7 @@ export class FooterComponent {
 
   getText(): string {
     const today = new Date();
-    return `© ${today.getFullYear()} All Rights Reserved by me.`;
+    return `© ${today.getFullYear()} All Rights Reserved by me. Beta Version 0.5`;
   }
 
 }
